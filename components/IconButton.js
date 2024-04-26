d583eeedc61f8863fbe16435a27e91d20de24e85
Lucas Domene <lucas.firmo@gmail.com>
2024-04-26 11:34:32 -0300

    fix: styles of IconButton

--- components/IconButton.js
+++ components/IconButton.js
@@ -3,9 +3,9 @@ import MaterialIcons from '@expo/vector-icons/MaterialIcons';
 
 export default function IconButton({ icon, label, onPress }) {
   return (
-    <Pressable onPress={onPress}>
+    <Pressable style={styles.iconButton} onPress={onPress}>
       <MaterialIcons name={icon} size={24} color="#fff" />
-      <Text>{label}</Text>
+      <Text style={styles.iconButtonLabel}>{label}</Text>
     </Pressable>
   );
 }
